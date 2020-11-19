7f7f5b19ff8a106670b79ab7dd4656e573eb3011
tmddnjs1533 <tmddnjs1533@gmail.com>
2020-11-19 09:42:08 +0900

    회원가입

--- main.js
+++ main.js
@@ -20,6 +20,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//appliction/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
@@ -31,6 +38,22 @@ mongoose.connect('mongodb+srv://tmddnjs1533:5a6s7d8f@cluster0.nktlw.mongodb.net/
 
 app.get('/', (req, res) => {
   res.send('Hello World~~! 안녕하세요~~~!')
+})
+
+app.post("/register", (req, res) => {
+  /**
+   * 회원가입 할 때 필요한 정보들을 client에서 가져오면
+   * 그것들을 데이터 베이스에 넣어준다.
+   */
+
+  const user = new User(req.body)
+
+  user.save((err,useInfo) => {
+    if(err) return res.json({success: false, err})   
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
