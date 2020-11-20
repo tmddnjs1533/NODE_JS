fa5c51cf27ad2db27e5678cea4adc386a665ad74
Kimsw <tmddnjs1533@gmail.com>
2020-11-20 22:59:51 +0900

    nodemon

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://tmddnjs1533:5a6s7d8f@cluster0.nktlw.mongodb.net/newNodeJS?retryWrites=true&w=majority', {
@@ -13,7 +21,20 @@ mongoose.connect('mongodb+srv://tmddnjs1533:5a6s7d8f@cluster0.nktlw.mongodb.net/
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~안녕하세요')
+  res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  user.save((err,doc) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
